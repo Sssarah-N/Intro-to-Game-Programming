88585ffc7d182e3c9922801d1db7659b4bdcd2bb
Sssarah-N <ruohanniu@gmail.com>
2025-11-08 21:57:39 -0500

    added header

--- Project4/main.cpp
+++ Project4/main.cpp
@@ -1,3 +1,14 @@
+/**
+* Author: Sarah Niu
+* Assignment: Rise of the AI
+* Date due: 2025-11-08, 11:59pm
+* I pledge that I have completed this assignment without
+* collaborating with anyone else, in conformance with the
+* NYU School of Engineering Policies and Procedures on
+* Academic Misconduct.
+**/
+
+
 #include "CS3113/LevelC.h"
 #include "CS3113/startScreen.h"
 #include "CS3113/wonScene.h"
